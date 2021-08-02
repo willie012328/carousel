67a448d3a8796f5abb60c617ddd9c555469f800a
willie012328 <q012328@gmail.com>
2021-08-02 04:36:26 -0500

    update error page button style

--- src/containers/error/index.jsx
+++ src/containers/error/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ErrorgPage = () => {
+const ErrorgPage = (props) => {
   return (
     <section className='app'>
       <div className='app-sandbox'>
         <div className='app-sandbox-content text-center'>
           <h2 className='app-header'>Error</h2>
+          <h2 className='app-header'>Code: {props.status}</h2>
 
           <div className='margin-auto'>
+            <p>Error Content: {props.data}</p>
             <p>Please make sure source repository exists or check out you network connection.</p>
+            <Link className="button-like-link" to="/" replace>Back to Landing Page</Link>
           </div>
         </div>
 
